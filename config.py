9c34b587822e3fd2f44db9f22eb146c3888f83d7
Sai Sun <saisun.ss@gmail.com>
2019-04-11 12:07:31 +1000

    Moved meta data into config.py and fixed the bug of removing element from list
    The bug,
    When looping a list, removing an element will cause the next element been skipped

---
+++ config.py
@@ -0,0 +1,115 @@
+mmCoursesMeta = {
+    'buslead': {
+        "buslead1x": {"title": "UQx_BUSLEAD1x",  'grade': 0.65},
+        "buslead2x": {"title": "UQx_BUSLEAD2x",  'grade': 0.65},
+        "buslead3x": {"title": "UQx_BUSLEAD3x",  'grade': 0.65},
+        "buslead4x": {"title": "UQx_BUSLEAD4x",  'grade': 0.65},
+        "buslead5x": {"title": "UQx_BUSLEAD5x",  'grade': 0.65},
+    },
+    'corpinn': {
+        "corpinn1x": {"title": "UQx_CORPINN1x", 'grade': 0.7},
+        "corpinn2x": {"title": "UQx_CORPINN2x",  'grade': 0.7},
+        "corpinn3x": {"title": "UQx_CORPINN3x",  'grade': 0.7},
+        "corpinn4x": {"title": "UQx_CORPINN4x",  'grade': 0.7},
+        "corpinn5x": {"title": "UQx_CORPINN5x",  'grade': 0.5},
+    },
+    'engy': {
+        "engy0x": {"title": "UQx_ENGY0x", 'grade': 0.7},
+        "engy1x": {"title": "UQx_ENGY1x", 'grade': 0.7},
+        "engy2x": {"title": "UQx_ENGY2x", 'grade': 0.7},
+        "engy3x": {"title": "UQx_ENGY3x", 'grade': 0.7},
+        "engycapx": {"title": "UQx_ENGYCAPx", 'grade': 0.7},
+    },
+    'lgdm': {
+        "lgdm1x": {"title": "UQx_LGDM1x", 'grade': 0.65},
+        "lgdm2x": {"title": "UQx_LGDM2x",  'grade': 0.65},
+        "lgdm3x": {"title": "UQx_LGDM3x",  'grade': 0.65},
+        "lgdm4x": {"title": "UQx_LGDM4x",  'grade': 0.65},
+        "lgdm5x": {"title": "UQx_LGDM5x",  'grade': 0.65},
+    },
+}
+
+
+mmCoursesSeries = {
+    'buslead': ['buslead1x', 'buslead2x', 'buslead3x', 'buslead4x'],
+    'corpinn': ['corpinn1x', 'corpinn2x', 'corpinn3x', 'corpinn4x'],
+    'engy': ['engy0x', 'engy1x', 'engy2x', 'engy3x'],
+    'lgdm': ['lgdm1x', 'lgdm2x', 'lgdm3x', 'lgdm4x'],
+}
+
+pointed_course_instances = {
+    'lgdm1x': {
+        'current_run': {'title': 'Leaders in Global Development', 'link': 'https://www.edx.org/course/leaders-in-global-development', 'instance_code': 'UQx_LGDM1x_1T2019'},
+        'coming_run': {'title': 'Leaders in Global Development', 'link': 'https://www.edx.org/course/leaders-in-global-development', 'instance_code': 'UQx_LGDM1x_1T2019'},
+    },
+    'lgdm2x': {
+        'current_run':{'title': 'The Science and Practice of Sustainable Development', 'link': 'https://www.edx.org/course/the-science-and-practice-of-sustainable-development', 'instance_code': 'UQx_LGDM2x_1T2019'},
+        'coming_run': {'title': 'The Science and Practice of Sustainable Development', 'link': 'https://www.edx.org/course/the-science-and-practice-of-sustainable-development', 'instance_code': 'UQx_LGDM2x_1T2019'},
+    },
+    'lgdm3x': {
+        'current_run':{'title': 'Adaptive Leadership in Development', 'link': 'https://www.edx.org/course/adaptive-leadership-in-development', 'instance_code': 'UQx_LGDM3x_1T2018'}, 
+        'coming_run': {'title': 'Adaptive Leadership in Development', 'link': 'https://www.edx.org/course/adaptive-leadership-in-development', 'instance_code': 'UQx_LGDM3x_1T2018'},       
+    },
+    'lgdm4x': {
+        'current_run':{'title': 'Critical Development Perspectives', 'link': 'https://www.edx.org/course/critical-development-perspectives', 'instance_code': 'UQx_LGDM4x_1T2019'},
+        'coming_run': {'title': 'Critical Development Perspectives', 'link': 'https://www.edx.org/course/critical-development-perspectives', 'instance_code': 'UQx_LGDM4x_1T2019'},        
+    },
+    'corpinn1x': {
+        'current_run':{'title': 'Design Thinking and Creativity for Innovation', 'link': 'https://www.edx.org/course/design-thinking-and-creativity-for-innovation-0', 'instance_code': 'UQx_CORPINN1x_1T2019'},
+        'coming_run': {'title': 'Design Thinking and Creativity for Innovation', 'link': 'https://www.edx.org/course/design-thinking-and-creativity-for-innovation-0', 'instance_code': 'UQx_CORPINN1x_1T2019'},        
+    },
+    'corpinn2x': {
+        'current_run':{'title': 'Idea Development: Create and Implement Innovative Ideas', 'link': 'https://www.edx.org/course/idea-development-create-and-implement-innovative-ideas', 'instance_code': 'UQx_CORPINN2x_1T2019'},
+        'coming_run': {'title': 'Idea Development: Create and Implement Innovative Ideas', 'link': 'https://www.edx.org/course/idea-development-create-and-implement-innovative-ideas', 'instance_code': 'UQx_CORPINN2x_1T2019'},        
+    },
+    'corpinn3x': {
+        'current_run':{'title': 'Innovation: From Plan to Product', 'link': 'https://www.edx.org/course/innovation-from-plan-to-product', 'instance_code': 'UQx_CORPINN3x_1T2019'},
+        'coming_run': {'title': 'Innovation: From Plan to Product', 'link': 'https://www.edx.org/course/innovation-from-plan-to-product', 'instance_code': 'UQx_CORPINN3x_1T2019'},        
+    },
+    'corpinn4x': {
+        'current_run':{'title': 'Creating and Sustaining an Innovation Culture', 'link': 'https://www.edx.org/course/creating-sustaining-innovation-culture-uqx-corpinn4x', 'instance_code': 'UQx_CORPINN3x_1T2019'},
+        'coming_run': {'title': 'Creating and Sustaining an Innovation Culture', 'link': 'https://www.edx.org/course/creating-sustaining-innovation-culture-uqx-corpinn4x', 'instance_code': 'UQx_CORPINN3x_1T2019'},        
+    },
+    'buslead1x': {
+        'current_run':{'title': 'Becoming an Effective Leader', 'link': 'https://www.edx.org/course/becoming-an-effective-leader-0', 'instance_code': 'UQx_BUSLEAD1x_1T2019'},
+        'coming_run': {'title': 'Becoming an Effective Leader', 'link': 'https://www.edx.org/course/becoming-an-effective-leader-0', 'instance_code': 'UQx_BUSLEAD1x_1T2019'},        
+    },
+    'buslead2x': {
+        'current_run':{'title': 'Leading the Organization', 'link': 'https://www.edx.org/course/leading-the-organization-0', 'instance_code': 'UQx_BUSLEAD2x_1T2019'},
+        'coming_run': {'title': 'Leading the Organization', 'link': 'https://www.edx.org/course/leading-the-organization-0', 'instance_code': 'UQx_BUSLEAD2x_1T2019'},        
+    },
+    'buslead3x': {
+        'current_run':{'title': 'Leading High-Performing Teams', 'link': 'https://www.edx.org/course/leading-high-performing-teams', 'instance_code': 'UQx_BUSLEAD3x_1T2019'},
+        'coming_run': {'title': 'Leading High-Performing Teams', 'link': 'https://www.edx.org/course/leading-high-performing-teams', 'instance_code': 'UQx_BUSLEAD3x_1T2019'},        
+    },
+    'buslead4x': {
+        'current_run':{'title': 'Leading in a Complex Environment', 'link': 'https://www.edx.org/course/leading-in-a-complex-environment', 'instance_code': 'UQx_BUSLEAD4x_1T2019'},
+        'coming_run': {'title': 'Leading in a Complex Environment', 'link': 'https://www.edx.org/course/leading-in-a-complex-environment', 'instance_code': 'UQx_BUSLEAD4x_1T2019'},        
+    },
+    'engy0x': {
+        'current_run':{'title': 'Energy Principles and Renewable Energy', 'link': 'https://www.edx.org/course/energy-principles-and-renewable-energy-2', 'instance_code': 'UQx_ENGY0x_1T2019'},
+        'coming_run': {'title': 'Energy Principles and Renewable Energy', 'link': 'https://www.edx.org/course/energy-principles-and-renewable-energy-2', 'instance_code': 'UQx_ENGY0x_1T2019a'},        
+    },
+    'engy1x': {
+        'current_run':{'title': 'Climate Science and Policy', 'link': 'https://www.edx.org/course/climate-science-and-policy-2', 'instance_code': 'UQx_ENGY1x_1T2019'},
+        'coming_run': {'title': 'Climate Science and Policy', 'link': 'https://www.edx.org/course/climate-science-and-policy-2', 'instance_code': 'UQx_ENGY1x_1T2019a'},        
+    },
+    'engy2x': {
+        'current_run':{'title': 'Energy and Development', 'link': 'https://www.edx.org/course/energy-and-development-1', 'instance_code': 'UQx_ENGY2x_1T2019'},
+        'coming_run': {'title': 'Energy and Development', 'link': 'https://www.edx.org/course/energy-and-development-1', 'instance_code': 'UQx_ENGY2x_2T2019'},        
+    },
+    'engy3x': {
+        'current_run':{'title': 'Low Emission Technologies and Supply Systems', 'link': 'https://www.edx.org/course/low-emission-technologies-and-supply-systems-0', 'instance_code': 'course-v1:UQx_ENGY3x_1T2019'},
+        'coming_run': {'title': 'Low Emission Technologies and Supply Systems', 'link': 'https://www.edx.org/course/low-emission-technologies-and-supply-systems-0', 'instance_code': 'course-v1:UQx_ENGY3x_2T2019'},        
+    },    
+}  
+
+from_address = '"UQx Updates" <business@uqx.uq.edu.au>'
+subject = 'Congratulations - you are now one step closer to the MicroMasters credential!'
+
+template_files = {
+    'buslead': 'buslead_dynamic',
+    'corpinn': 'corpinn_dynamic',
+    'engy': 'engy_dynamic',
+    'lgdm': 'lgdm_dynamic'  
+}
